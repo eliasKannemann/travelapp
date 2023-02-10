5fe27ed5856149182b756f8f846f4107472f03cf
Elias Kanneman <elias_gk1@hotmail.com>
2023-02-09 22:13:38 -0300

    created explore component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
-import { Navbar, Hero, Footer, Memory } from "./components";
-import { hero, navlinks, memory } from "./data/traveldata";
+import { Navbar, Hero, Footer, Memory, Explore } from "./components";
+import { hero, navlinks, memory, placesAPI } from "./data/traveldata";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +10,7 @@ function App() {
       <Navbar navlinks={navlinks} />
       <Hero hero={hero} />
       <Memory memory={memory} />
+      <Explore title="explore the world" placesAPI={placesAPI} />
       <Footer />
     </>
   );
